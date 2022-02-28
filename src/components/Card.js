03052cbd35f57534e38a18a6da5e2d1ddb9dccbc
march0320 <march02198@gmail.com>
2022-02-28 23:30:58 +0900

    change url

--- src/components/Card.js
+++ src/components/Card.js
@@ -24,7 +24,7 @@ export default function Card({ value }) {
         >
           <DetailedShop onOpen={onOpen} isOpen={isOpen} onClose={onClose} value={value}/>
           {`../../public/img_res/${value.name}/1.png` ? (
-            <Image src={`../../public/img_res/${value.name}/1.png`.src} objectFit="fill" maxW="15vw" maxH="13vw" w="15vw" h="13vw" />
+            <Image src={`https://beve2-march0320.vercel.app/img_res/${value.name}/1.png`.src} objectFit="fill" maxW="15vw" maxH="13vw" w="15vw" h="13vw" />
           ):(<Image src={noimage.src} objectFit="fill" maxW="15vw" maxH="13vw" w="15vw" h="13vw" />)
           }
           <Box m={2} textAlign="left">
