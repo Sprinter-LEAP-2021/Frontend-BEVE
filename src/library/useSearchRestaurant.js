9800782a3f9a9e600a62714e3476be874e393a67
march0320 <march02198@gmail.com>
2022-02-28 22:37:25 +0900

    change api link

--- src/library/useSearchRestaurant.js
+++ src/library/useSearchRestaurant.js
@@ -2,7 +2,13 @@ import { useEffect, useState } from "react";
 import { useRecoilValue, useRecoilState } from "recoil";
 import axios from "axios";
 
-import { filterState, pageState, queryState, restaurantDataState, inputState } from "../states";
+import {
+  filterState,
+  pageState,
+  queryState,
+  restaurantDataState,
+  inputState,
+} from "../states";
 
 export default function useSearchRestaurants() {
   const [isLoading, setIsLoading] = useState(null);
@@ -10,28 +16,32 @@ export default function useSearchRestaurants() {
 
   const query = useRecoilValue(queryState);
   const page = useRecoilValue(pageState);
-  const [restaurantData, setRestaurantData] = useRecoilState(restaurantDataState);
+  const [restaurantData, setRestaurantData] =
+    useRecoilState(restaurantDataState);
   const [filter, setFilter] = useRecoilState(filterState);
   const [inputText, setInputText] = useRecoilState(inputState);
-  
+
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
       setIsError(false);
       console.log(query);
 
-      const response = await axios.get(`https://beve-deploy.vercel.app/api/posts?area=${query.area}&stage=${query.stage}&type=${query.type}&inputText=${query.inputText}&page=${page}`)
-        .catch(error => {
-        setIsError(true);
-        console.log(error.response.data);
-      }).then(response => {
-        setIsLoading(false);
-        setRestaurantData(response.data);
-      })
+      const response = await axios
+        .get(
+          `https://beve1-march0320.vercel.app//api/posts?area=${query.area}&stage=${query.stage}&type=${query.type}&inputText=${query.inputText}&page=${page}`
+        )
+        .catch((error) => {
+          setIsError(true);
+          console.log(error.response.data);
+        })
+        .then((response) => {
+          setIsLoading(false);
+          setRestaurantData(response.data);
+        });
     };
-    
-    fetchData();
 
+    fetchData();
   }, [query, page]);
 
   return { isLoading, isError };
