caadade27583f4f614465946daba7f0afb842db4
march0320 <march02198@gmail.com>
2022-03-05 15:38:08 +0900

    Fix: detail fran image

--- src/components/Card.js
+++ src/components/Card.js
@@ -49,12 +49,8 @@ export default function Card({ value }) {
           color="black"
           onClick={onOpen}
         >
-          <DetailedShop onOpen={onOpen} isOpen={isOpen} onClose={onClose} value={value}/>
-        
+          <DetailedShop onOpen={onOpen} isOpen={isOpen} onClose={onClose} value={value} franName={tmp_t[0]}/>        
              <Image src={`https://beve2-march0320.vercel.app/img_res/` + tmp_t[0] + `/1.PNG`} onError="this.src='https://beve2-march0320.vercel.app/img/noimage.jpg';" objectFit="fill" maxW="15vw" maxH="13vw" w="15vw" h="13vw" />
-         
-
-
           <Box m={2} textAlign="left">
             <Text color="green" fontWeight="bolder">{value.name}</Text>
             <Text fontSize="0.8rem" justifySelf="end">{value.category}</Text>
--- src/components/DetailedShop.js
+++ src/components/DetailedShop.js
@@ -7,7 +7,7 @@ import React, { useState } from "react";
 import Map from "./Map";
 import {  VscCircleOutline,VscCircleFilled } from "react-icons/vsc";
 
-export default function DetailedShop({ value, isOpen, onClose }) {
+export default function DetailedShop({ value, isOpen, onClose,franName }) {
   
   const [count, setCount] = useState(1);
 
@@ -20,21 +20,29 @@ export default function DetailedShop({ value, isOpen, onClose }) {
     else setCount(2);
   };
 
-  if (count == 1) {
     return (
       <>
         <Modal size="xl" isOpen={isOpen} onClose={onClose}>
           <ModalOverlay color="green" />
           <ModalContent>
-            <Image
-              w="50rem"
-              h="30rem"
-              boxShadow="md"
-              borderRadius="0.2rem"
-              src={`https://beve2-march0320.vercel.app/img_res/${value.name}/${count}.PNG`}
-              objectFit="fill"
-            />
+            {value.franchise == 0?
+              <Image
+                w="50rem"
+                h="30rem"
+                boxShadow="md"
+                borderRadius="0.2rem"
+                src={`https://beve2-march0320.vercel.app/img_res/${value.name}/${count}.PNG`}
+                objectFit="fill"
+              />:  <Image
+                w="50rem"
+                h="30rem"
+                boxShadow="md"
+                borderRadius="0.2rem"
+                src={`https://beve2-march0320.vercel.app/img_res/${franName}/1.PNG`}
+              />}
             <Flex direction="row" justify="center" m={1}>
+              {count == 1 ?
+                <>
               <Button
                 p={1}
                 as={VscCircleFilled}
@@ -50,6 +58,21 @@ export default function DetailedShop({ value, isOpen, onClose }) {
                 variant="ghost"
                 color="#3E603B"
               />
+              </>:<><Button
+                p={1}
+                as={VscCircleOutline}
+                onClick={before}
+                size="sm"
+                variant="ghost"
+                color="#3E603B"
+              />
+              <Button
+                p={1}
+                as={VscCircleFilled}
+                size="sm"
+                variant="ghost"
+                color="#3E603B"
+              /></> }
             </Flex>
             <ModalCloseButton borderRadius="3rem" backgroundColor="#3E603B" color="white" />
             <ModalBody>
@@ -82,68 +105,7 @@ export default function DetailedShop({ value, isOpen, onClose }) {
         </Modal>
       </>
     );
-  } else {
-    return (
-      <>
-        <Modal size="xl" isOpen={isOpen} onClose={onClose}>
-          <ModalOverlay color="green" />
-          <ModalContent>
-            <Image
-              w="50rem"
-              h="30rem"
-              boxShadow="md"
-              borderRadius="0.2rem"
-              src={`https://beve2-march0320.vercel.app/img_res/${value.name}/${count}.PNG`}
-              objectFit="fill"
-            />
-            <Flex direction="row" justify="center" m={1}>
-              <Button
-                p={1}
-                as={VscCircleOutline}
-                onClick={before}
-                size="sm"
-                variant="ghost"
-                color="#3E603B"
-              />
-              <Button
-                p={1}
-                as={VscCircleFilled}
-                size="sm"
-                variant="ghost"
-                color="#3E603B"
-              />
-            </Flex>
-            <ModalCloseButton borderRadius="3rem" backgroundColor="#3E603B" color="white" />
-            <ModalBody>
-              <Flex direction="column" justify="center">
-                <Text fontSize="1.8rem" fontWeight="bolder" m={2}>{value.name}</Text>
-                <Box m={2}>
-                  <Text m={1}>카테고리 : {value.category}</Text>
-                  <Text m={1}>전화번호 : {value.phone}</Text>
-                  <Text m={1}>주소 : {value.location.address}</Text>
-                </Box>
-                <Box m={2}>
-                  <Text m={1}>메뉴<hr /></Text>
-                  <Text>
-                    {value.menu.map((menus, index) => (
-                      <Flex key={index}>
-                        <Text m={0.5}>{menus.name}</Text>
-                        <Spacer />
-                        <Text>{menus.level.join(", ")}</Text>
-                      </Flex>
-                    ))}
-                  </Text>
-                </Box>
-              </Flex>
-              <ModalCloseButton borderRadius="3rem" backgroundColor="#3E603B" color="white" />
-              <Flex justify="center" m={4}>
-                <Map value={value} />
-              </Flex>
-            </ModalBody>
-          </ModalContent>
-        </Modal>
-      </>
-    );       
-  }
-}
+  }        
+  
+
 
